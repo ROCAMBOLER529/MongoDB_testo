34e844cf9ba53d3f1677b53902c46bd569e05b5b
ROCAMBOLER529 <42423816+ROCAMBOLER529@users.noreply.github.com>
2022-10-11 12:08:40 -0300

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,8 @@ const app = express();
 
 // se llama a persona.js
 app.use(require("./persona.js"));
-
-
+app.use(require("./reclamos.js"));
+app.use(require("./login.js"));
 
 // se exporta el archivo index
 module.exports = app;
