59554bb0254ae78535a5237ad5ad9dbc5adfddc8
ROCAMBOLER529 <42423816+ROCAMBOLER529@users.noreply.github.com>
2022-10-11 08:06:16 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,26 +1,47 @@
-// npm install -> instala.
-
 const express = require("express");
-const app = express();
-const PORT = 3000;
 const mongoose = require("mongoose");
+const hbs = require("hbs");
+// Middleware necesario para parsear parametros del POST.
+const bodyParser = require('body-parser');
 
-// import persona.js
-app.use(require("./routes/index.js"));
+const port = 3000;
+const app = express();
 
-// nodemon -> 
-// json: todos los campos van separados por comas MENOS el ultimo
+app.use(express.static("public"));
 
-const mongoDB = `mongodb://localhost/municipio`;
-mongoose.connect(mongoDB, {}, (err) => {
-    if (err) {
-        console.log("Hubo un error en: ", err);
-    } else {
-        console.log("Se conecto!");
-    }
-})
+app.set("view engine", "hbs");
+hbs.registerPartials("./views/partials");
 
-app.listen(PORT, () => {
-    console.log(`Escuchando en el puertoooooo ${PORT}...`);
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
+
+app.use(require("./routes/index")); //importante que este esto DESPUES de urlencoded
+
+let mongodb = "mongodb://localhost:27017/municipio";
+
+mongoose.connect(mongodb, {}, (err) => {
+  if (err) {
+    console.error("Error de conexion con la BDD", err);
+  }
 });
 
+//Rutas
+
+app.get("/", (req, res) => {
+  res.render("home.hbs");
+});
+
+app.get("/usuario/administrador", (req, res) => {
+  res.render("administrador.hbs");
+});
+
+app.get("/usuario/contribuyente", (req, res) => {
+  res.render("contribuyente.hbs");
+});
+
+app.listen(port, () => {
+  console.log(`Escuchando en el puerto ${port}`);
+});
