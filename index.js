9220324e0c73a2069122ea57e59eb1a517bc2c5e
ROCAMBOLER529 <42423816+ROCAMBOLER529@users.noreply.github.com>
2022-10-18 03:39:59 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-require("./config/config.js");
+
+/*
+  npm install --save nodemon
+  npm install --save express
+  npm install --save mongooose
+  npm install --save hbs
+  npm install --save body-parser
+  npm install --save bcryptjs
+  npm install --save jsonwebtokens
+  npm start
+
+  Postman:
+    localhost:3000/...
+*/
+
+
 const express = require("express");
-const mongoose = require("mongoose");
+require("./config/config.js");
+const app = express();
+const mongoose = require('mongoose');
+
 const hbs = require("hbs");
 // Middleware necesario para parsear parametros del POST.
 const bodyParser = require('body-parser');
-
-const app = express();
+const bcrypt = require('bcryptjs');
+const jsw = require("jsonwebtoken");
 
 app.use(express.static("public"));
 
@@ -16,32 +34,35 @@ hbs.registerPartials("./views/partials");
 app.use(bodyParser.urlencoded({ extended: false }));
 
 // parse application/json
-app.use(bodyParser.json());
+ app.use(bodyParser.json());
 
 app.use(require("./routes/index")); //importante que esto DESPUES de urlencoded
 
-let mongodb = "mongodb://localhost:27017/municipio";
+const mongodb = "mongodb://0.0.0.0:27017/municipalidad";
 
-mongoose.connect(mongodb, {}, (err) => {
-  if (err) {
-    console.error("Error de conexion con la BDD", err);
-  }
-});
+mongoose.connect(mongodb, (err) => {
+    if(err){
+        console.log("Ocurrio un error ", err);
+    }else{
+        console.log("Conexion exitosa");
+    }
+})
 
 //Rutas
 
 app.get("/", (req, res) => {
-  res.render("home.hbs");
-});
+   res.render("home.hbs");
+ });
 
-app.get("/usuario/administrador", (req, res) => {
-  res.render("administrador.hbs");
-});
+ app.get("/usuario/administrador", (req, res) => {
+   res.render("administrador.hbs");
+ });
 
 app.get("/usuario/contribuyente", (req, res) => {
-  res.render("contribuyente.hbs");
+  res.render("contribuyentes.hbs");
 });
 
 app.listen(process.env.PORT, () => {
-  console.log(`Escuchando en el puerto ${process.env.PORT}`);
+   console.log(`Escuchando en el puerto ${process.env.PORT}`);
 });
+
