baaf4ab404ff475c8fb5aa01db60170de6881842
ROCAMBOLER529 <42423816+ROCAMBOLER529@users.noreply.github.com>
2022-10-25 02:09:21 -0300

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,3 +1,8 @@
+/*
+    Casa: PORT = 27012;
+    Uni: PORT = 3000;
+*/
+
 process.env.PORT = process.env.PORT || 3000;
 process.env.CLAVE_SECRETA = `*****633b56f63f0e7ee5d31ad7dc*****`;
 process.env.TIEMPO_DE_CADUCIDAD = 1000 * 60;
