9f77663a336082909e67c3d8d95ce63473f697be
ROCAMBOLER529 <42423816+ROCAMBOLER529@users.noreply.github.com>
2022-10-25 02:11:37 -0300

    Update persona.js

--- models/persona.js
+++ models/persona.js
@@ -1,12 +1,12 @@
-// npm install --save mongoose
-
 const mongoose = require(`mongoose`); // se declara a mongoose
 const persona_schema = new mongoose.Schema({
     nombre: String, // forma corta
     apellido: {type: String}, // forma larga
     dni: String,
     mail: String,
-    movil: String
+    telefono: String,
+    password: String,
+    rol: String
     /* 
     isActive: Boolean,
     cantHijos: Number,
