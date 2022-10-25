61139e4bfe28f40a86b718abd9d07320e6a7d2f7
ROCAMBOLER529 <42423816+ROCAMBOLER529@users.noreply.github.com>
2022-10-25 02:13:53 -0300

    Update persona.js

--- routes/persona.js
+++ routes/persona.js
@@ -1,93 +1,121 @@
 const express = require("express");
 const app = express();
 const Persona = require("../models/persona.js"); // importa los modelos de persona.js
+const {verificarRol, verificarToken } = require("../middleware/autenticacion.js");
 
-app.get("/personas/all", (req, res) => {
-    Persona.find().exec((err, personas_data) => {
-        if (err) {
-            res.status(500).json({
-                res: "failed",
-                err
-            });
-        } else {
-            res.json({
-                resu: "ok",
-                personas: personas_data
-            });
-        }
-    });
+app.get("/personas/all", verificarRol, (req, res) => {
+  Persona.find().exec((err, personas_data) => {
+    if (err) {
+      res.status(500).json({
+        res: "failed",
+        err,
+      });
+    } else {
+      res.json({
+        resu: "ok",
+        personas: personas_data,
+      });
+    }
+  });
 });
 
-app.get(`/personas/:id`, (req, res) => {
-    const id = req.params.id; // id de personas/${id} 
-     Persona.findOne ({ _id: id }).exec((err, persona_data) => {
-        if (err) {
-            res.status(500).json({
-                resu: "failed",
-                err
-            });
-        } else {
-            res.json({
-                resu: "ok",
-                personas: persona_data
-            });
-        }
-    });
+app.get("/personas/:nombre", verificarRol, (req, res) => {
+  // id de personas/${id}
+  Persona.find({ nombre: req.params.nombre }).exec((err, data) => {
+    if (err) {
+      res.status(500).json({
+        resu: "failed",
+        err,
+      });
+    } else {
+      res.json({
+        resu: "ok",
+        personas: data,
+      });
+    }
+  });
 });
 
-app.post("/personas/add", async (req, res) => {
-    let body = req.body;
-    console.log(body);
+app.post("/personas/add", verificarRol, async (req, res) => {
+  let body = req.body;
+  console.log(body);
 
-	let persona = new Persona({
-		nombre: body.nombre,
-		apellido: body.apellido,
-		DNI: body.DNI,
-		email: body.email,
-    telefono: body.telefono
-	});
- 
+  let persona = new Persona({
+    nombre: body.nombre,
+    apellido: body.apellido,
+    password: body.password,
+    dni: body.dni,
+    mail: body.mail,
+    telefono: body.telefono,
+    rol: body.rol
+  });
+
   console.log(persona);
 
   res.json({
-      ok: true,
-      persona: persona
-    });
+    ok: true,
+    persona: persona,
+  });
 
-		try{
-      let personaDB = await persona.save();
-    } catch(err) {
-			// Envia codigo de error 400
-			res.status(400).json({
-				ok: false,
-				err
-			});
-		} 
-	});
-
-app.put("/personas/edit", (req, res) => {
-    res.json({
-        resu: "ok",
-        personas: {}
+  try {
+    let personaDB = await persona.save();
+  } catch (e) {
+    res.status(400).json({
+      ok: false,
+      err,
     });
+  }
 });
 
-app.delete("/persona/delete/:id", (req, res) => {
-  Persona.remove({ _id: req.params.id }).exec((error, data) => {
-    if (error) {
+app.put("/personas/edit/:id", verificarRol, async (req, res) => {
+  let body = req.body;
+  
+  Persona.updateOne(
+    { _id: req.params.id },
+    {
+      $set: {
+        nombre: body.nombre,
+        apellido: body.apellido,
+        dni: body.dni,
+        mail: body.mail,
+        telefono: body.telefono,
+        rol: body.rol,
+        password: body.password
+      },
+    }
+  ).exec((err, persona_data) => {
+    if (err) {
       res.status(500).json({
-        resul: "fallo",
-        error,
+        resu: "failed",
+        err,
+      });
+    } else {
+      res.json({
+        resu: "ok",
+        persona: persona_data,
+      });
+    }
+  });
+});
+
+app.delete("/personas/delete/:id", verificarRol, (req, res) => {
+  let id = req.params.id;
+
+  Persona.deleteOne({ _id: id }).exec((err, persona_data) => {
+    if (err) {
+      res.status(500).json({
+        resu: "fallo",
+        err,
       });
     }
     res.json({
-      resul: "ok",
-      persona: data,
+      resu: "ok",
+      persona: persona_data,
     });
   });
 });
 
-//export 
+//export
 module.exports = app;
 
 /* 
