88a72ecdb2e11c6df1961b063e0615b5a4101751
ROCAMBOLER529 <42423816+ROCAMBOLER529@users.noreply.github.com>
2022-10-25 02:12:46 -0300

    Add files via upload

---
+++ models/reclamos.js
@@ -0,0 +1,13 @@
+const mongoose = require(`mongoose`); // se declara a mongoose
+const reclamos_schema = new mongoose.Schema({
+  fecha_creacion: String,
+  fecha_resuelta: String,
+  categoria: String,
+  domicilio: String,
+});
+
+if (!reclamos_schema.fecha_resuelta) reclamos_schema.fecha_resuelta = "No se resolvió";
+
+const Reclamo = mongoose.model("reclamos", reclamos_schema);
+
+module.exports = Reclamo;
