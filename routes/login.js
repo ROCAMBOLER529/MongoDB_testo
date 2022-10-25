4677d3d4d4b3d5cf9e93dab9ea84d8f35d8fe504
ROCAMBOLER529 <42423816+ROCAMBOLER529@users.noreply.github.com>
2022-10-25 02:13:34 -0300

    Update login.js

--- routes/login.js
+++ routes/login.js
@@ -1,19 +1,71 @@
 const express = require("express");
+const bcrypt = require("bcryptjs");
+var salt = bcrypt.genSaltSync(10);
 const app = express();
-const Persona = require("../models/persona.js"); // importa los modelos de persona.js
+const Persona = require("../models/persona.js");
+const crearUsuario = require("../helpers/encriptador.js");
+const generarJWT = require("../helpers/generador_JWT.js");
+const verificarSignUp = require("../middleware/signUp.js");
 
-app.post(`/personas/login`, async (req, res) => {
-    const {Uid, mail, password} = req.body; // id de personas/${id} 
-    Persona.findOne({_id: Uid}).exec(async (err, data) => {
-        if (err) {
-            res.status(400).json({
-                resu: "Usuario no existente",
-                err
-            });
-        } else {
-             
-        }
+
+
+app.post(`/personas/login`, (req, res) => {
+  const { mail, password } = req.body;
+  // const usuario = crearUsuario(req.body);
+
+  Persona.findOne({ mail: mail }).exec(async (err, data) => {
+    let contra = bcrypt.hashSync(data.password, salt);
+
+    if (err) {
+      res.status(500).json({
+        resu: "failed",
+        err,
+      });
+    } else if (bcrypt.compareSync(password, contra)) {
+      const token = await generarJWT(data.id);
+        res.json({
+        resu: "ok",
+        data,
+        token
+      });
+    } else {
+        res.status(400).json({
+            resu: "mail o contraseña incorrectos. Si no tiene una cuenta, por favor loguearse",
+            mail,
+            password
+        });
+    }
+  });
+});
+
+app.put(`/personas/signup`, verificarSignUp, async (req, res) => {
+    body = req.body;
+    var salt = bcrypt.genSaltSync(10);
+    const contra = bcrypt.hashSync(body.password, salt);
+
+    let nuevaPersona = new Persona({
+        dni: "",
+        nombre: "",
+        apellido: "",
+        mail: body.mail,
+        telefono: "",
+        password: contra,
+        rol: "contribuyente"
     });
+
+    try {
+        let personaDB = await nuevaPersona.save();
+        res.json({
+            resu: "ok",
+            mail: body.mail,
+            password: contra,
+        });
+    } catch (err) {
+        res.status(500).json({
+            resu: "failed",
+            err,
+        });
+    }
 });
 
 // ¡¡ESTUDIAR ASYNCRONISMO!!
