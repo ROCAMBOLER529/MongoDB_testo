df0c4ec791e1aac360014c022836239412d71c5f
ROCAMBOLER529 <42423816+ROCAMBOLER529@users.noreply.github.com>
2022-11-01 03:27:51 -0300

    Update login.js

--- routes/login.js
+++ routes/login.js
@@ -6,16 +6,46 @@ const Persona = require("../models/persona.js");
 const crearUsuario = require("../helpers/encriptador.js");
 const generarJWT = require("../helpers/generador_JWT.js");
 const verificarSignUp = require("../middleware/signUp.js");
+const Usuario = require("../models/usuarios.js");
 
 
 
-app.post(`/personas/login`, (req, res) => {
+app.post(`/personas/login`, async (req, res) => {
   const { mail, password } = req.body;
-  // const usuario = crearUsuario(req.body);
 
+  try {
+    const data = await Usuario.findOne({ mail: mail });
+    let contra = bcrypt.hashSync(data.password, salt);
+    if (bcrypt.compareSync(password, contra)){
+      const token = await generarJWT(data.id);
+      res.json({
+        resu: "ok",
+        data, 
+        token,
+      });
+    } else {
+      res.status(400).json({
+        resu: "mail o contraseña incorrectos. Si no tiene una cuenta, por favor loguearse",
+        mail,
+        password,
+      });
+    }
+  } catch (err) {
+    res.status(500).json({
+      resu: "failed",
+      err,
+    })
+  }
+
+
+
+  /*
   Persona.findOne({ mail: mail }).exec(async (err, data) => {
     let contra = bcrypt.hashSync(data.password, salt);
 
+
+
+    
     if (err) {
       res.status(500).json({
         resu: "failed",
@@ -36,9 +66,13 @@ app.post(`/personas/login`, (req, res) => {
         });
     }
   });
+
+  */
 });
 
 app.put(`/personas/signup`, verificarSignUp, async (req, res) => {
+    
+  /*
     body = req.body;
     var salt = bcrypt.genSaltSync(10);
     const contra = bcrypt.hashSync(body.password, salt);
@@ -66,6 +100,7 @@ app.put(`/personas/signup`, verificarSignUp, async (req, res) => {
             err,
         });
     }
+    */
 });
 
 // ¡¡ESTUDIAR ASYNCRONISMO!!
